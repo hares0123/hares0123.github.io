e42c8b6b842f8f54a78f33164dfa398dc4dfdb39
Adan <darks@kali>
2023-07-27 14:05:01 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+ /*=================================video==================================================*/
+
+var counter = 1;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++
+    if(counter > 4){
+        counter = 1;
+    }
+},5000);
+
+
+const navbarToggle = document.querySelector('#menu-icon');
+const navbarMenu = document.querySelector('.navbar');
+
+navbarToggle.addEventListener('click', function() {
+  navbarMenu.classList.toggle('active');
+});
