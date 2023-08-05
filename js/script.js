e29667bd6ca18e8d7d056cb5dc9c7936dbfd8326
hares0123 <136670594+hares0123@users.noreply.github.com>
2023-08-05 18:05:49 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,6 +8,23 @@ setInterval(function(){
         counter = 1;
     }
 },5000);
+
+function appendToResult(value) {
+  document.getElementById('result').value += value;
+}
+
+function clearResult() {
+  document.getElementById('result').value = '';
+}
+
+function calculate() {
+  try {
+    const result = eval(document.getElementById('result').value);
+    document.getElementById('result').value = result;
+  } catch (error) {
+    document.getElementById('result').value = 'Error';
+  }
+}
 /*
 const navbarToggle = document.querySelector('#menu-icon');
 const navbarMenu = document.querySelector('.navbar');
